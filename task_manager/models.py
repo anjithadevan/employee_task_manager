e463c8256b7b79156d041cdbc2f8ef0f5841d996
anjitha devan <anjithadevan@gmail.com>
2020-07-22 21:06:22 +0530

    data base creation

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import User
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
+
+
+class Task(models.Model):
+    NEW = 'NEW'
+    INPROGRESS = 'IP'
+    COMPLETED = 'CT'
+    HOLD = 'HD'
+    STATUS_OF_TASKS = [(NEW, 'New'), (INPROGRESS, 'In progress'), (COMPLETED, 'Completed'), (HOLD, 'Hold')]
+
+    employee = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    created_at = models.DateTimeField()
+    modified_at = models.DateTimeField(default=datetime.now())
+    status = models.CharField(max_length=3, choices=STATUS_OF_TASKS, default=NEW)
+
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    heading = models.CharField(max_length=100)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    created_at = models.DateTimeField(default=datetime.now())
