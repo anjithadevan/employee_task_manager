78a71d88548ddf6eaee057ce8bde13d2fe96ef3f
anjitha devan <anjithadevan@gmail.com>
2020-07-22 23:19:50 +0530

    registered models to the admin

--- task_manager/models.py
+++ task_manager/models.py
@@ -20,9 +20,15 @@ class Task(models.Model):
     modified_at = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=3, choices=STATUS_OF_TASKS, default=NEW)
 
+    def __str__(self):
+        return '%s, %s' % (self.employee.username, self.name)
+        return self.employee.username
 
 class Comment(models.Model):
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     heading = models.CharField(max_length=100)
     description = models.CharField(max_length=200, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.task.name
